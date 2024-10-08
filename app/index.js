cfa55945fe6bf28a463a0dc49ef2747ba0fb1f91
Sourav Sarkar <75407895+souravcit2013@users.noreply.github.com>
2024-10-08 23:59:11 +0530

    index.js

--- app/index.js
+++ app/index.js
@@ -2,38 +2,38 @@
 // console.log('on browser')
 
 // function accepts any mumber of parameters
-function addNums (...args) {
+function addNums(...args) {
   // console.log(args)
 }
 
-addNums ([2], [3]);
-addNums ([2], [3], [4]);
-addNums (2, 3, 5, 66);
-addNums (2, 3, 3, 5, 100);
+// addNums ([2], [3]);
+addNums([2], [3], [4]);
+addNums(2, 3, 5, 66);
+addNums(2, 3, 3, 5, 100);
 
 // js class method
 class Maths {
-  constructor (num1, num2) {
+  constructor(num1, num2) {
     this.num1 = num1;
     this.num2 = num2;
   }
 
-  multiply () {
+  multiply() {
     return this.num1 * this.num2;
   }
 
   // js static method
-  static multiplyNums (num1, num2) {
+  static multiplyNums(num1, num2) {
     return num1 * num2;
   }
 }
 
-const action = new Maths (10, 20);
+const action = new Maths(10, 20);
 // console.log(action.multiply())
 // console.log(Maths.multiplyNums(11, 11))
 
 /* js prototype */
-function Sourav (name, city) {
+function Sourav(name, city) {
   this.name = name;
   this.city = city;
   this.greet = () => `Hi, this is ${this.name} from ${this.city}`;
@@ -43,27 +43,27 @@ Sourav.prototype.skills;
 Sourav.prototype.info = function () {
   return `Working in ${this.skills}`;
 };
-let souravProto = new Sourav ('ss', 'kol');
+let souravProto = new Sourav('ss', 'kol');
 souravProto.skills = 'react.js';
 // console.log(souravProto)
 // console.log(souravProto.info())
 
 /* js map */
-let a = new Map ();
+let a = new Map();
 /* add key values to map */
-a.set (1, 'one');
-a.set ({name: 'ss'}, 'Object');
-a.set (function name () {}, 'Function');
+a.set(1, 'one');
+a.set({ name: 'ss' }, 'Object');
+a.set(function name() { }, 'Function');
 // console.log(a)
 
 /* Iterate a map */
 let mp = [
   [1, 'one'],
-  [{name: 'ss'}, 'object'],
-  [function name () {}, 'function'],
+  [{ name: 'ss' }, 'object'],
+  [function name() { }, 'function'],
 ];
-mp = new Map (mp);
-for (let [key, value] of mp.entries ()) {
+mp = new Map(mp);
+for (let [key, value] of mp.entries()) {
   // console.log('key== ', key, '     value== ', value)
 }
 
@@ -74,7 +74,7 @@ var x = function (a) {
 // x (10);
 
 // named function expression
-var x1 = function x11 (a1) {
+var x1 = function x11(a1) {
   // console.log('a1-',a1)
 };
 // x1 (20);
@@ -94,14 +94,14 @@ const person = {
   lastName: 'Cruise',
 };
 
-const {firstName: myName, lastName=''} = person
+const { firstName: myName, lastName = '' } = person
 // console.log(myName, lastName);
 
 console.log('ssss')
 /** async await fetch */
-async function callData () {
-    console.log('sssssssssss')
-    const _response = await fetch('https://api.github.com/gists/public');
-    console.log(_response)
+async function callData() {
+  console.log('sssssssssss')
+  const _response = await fetch('https://api.github.com/gists/public');
+  console.log(_response)
 }
 callData();
